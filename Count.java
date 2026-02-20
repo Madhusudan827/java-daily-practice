33fe8a5576bbf9a98b628dcd0129f980db72bd7f
madhu sudan <madhu7002sudan@gmail.com>
2026-02-20 20:49:21 +0530

    count number of  words in a sentence.

--- Count.java
+++ Count.java
@@ -1,34 +1,9 @@
 public class Count{
-    public static void count(String s){
-        int len=s.length();
-        int vowels=0;
-        int consonents=0;
-        int digits=0;
-        int others=0;
-        for(int i=0;i<len;i++){
-            char k=s.charAt(i);
-            if(k>='a' && k<='z'){
-                
-               if(k=='a'|k=='e'|k=='o'|k=='i'|k=='u'){
-                vowels+=1;
-              }else {
-                consonents++;
-               }
-            
-            }else if(k>='0' && k<='9')
-              {
-               digits ++;
-            }else {
-                others++;
-            }
-            
-        }
-        System.out.println("consonents:"+consonents);
-          System.out.println("digits:"+digits);
-        System.out.println("vowels:"+vowels);
-        System.out.println("others:"+others);
+    public  static int countsWords(String str){
+       String[] words=str.split("\\s+");
+       return words.length;
     }
-    public static void main(String args[]){
-           count("word1202#@$%");
+    public static void main(String[] args) {
+        System.out.println(countsWords("my name  is  madhu . i am a student"));
     }
 }
