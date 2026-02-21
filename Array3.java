1693ed5f130a0f4ec283da5f0caa0317d993cc90
madhu sudan <madhu7002sudan@gmail.com>
2026-02-21 22:16:32 +0530

    (50-51)array problems

--- Array3.java
+++ Array3.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 public class Array3 {
     public static int largestandSmallest(int [] n){
         int max=Integer.MIN_VALUE;
@@ -35,11 +36,56 @@ public class Array3 {
         return average;
 
     }
+    public static void reverseofArray(int [] ar){
+       int i=0;
+       int j=ar.length-1;
+      
+      
+       while(i<j){
+           //Swap condition
+             int temp=ar[i];
+             ar[i]=ar[j];
+             ar[j]=temp;
+             i++;
+             j--;
+        }
+       for(int k=0;k<ar.length;k++){
+        System.out.println(ar[k]);
+       }
+    }
+    public static void duplicteElements(int []arr){
+        for(int i=0;i<arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
+                if((arr[i])==arr[j]){
+
+                    System.out.print(arr[j]+" ");
+                    break;
+                }
+            }
+        }
+        
+    }
+    public static void ascendingArray(int []arr){
+      
+        for(int i=0;i<arr.length-1;i++){
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[i]>arr[j]){
+                   arr[i]=arr[i]+arr[j];
+                  arr[j]=arr[i]-arr[j];
+                  arr[i]=arr[i]-arr[j];
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
     public static void main(String args[]){
-        int []n={1,2,3,4,5};
-        System.out.println("min:"+largestandSmallest(n));
-        System.out.println("sum of array="+sumofArray(n));
-        System.out.println("Average of array="+averageofArray(n));
+        int []n={100003,234567,678899,654221,999999,1};
+        // System.out.println("min:"+largestandSmallest(n));
+        // System.out.println("sum of array="+sumofArray(n));
+        // System.out.println("Average of array="+averageofArray(n));
+       // reverseofArray(n);
+        //duplicteElements(n);
+       ascendingArray(n);
         
     }
 }
